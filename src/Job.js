7b5a1ed9439f8d4856fdf09e8ec9b7fa7f33fcd1
Dylan Plaster <dylanplaster1@gmail.com>
2021-03-25 09:58:27 -0400

    updated job styles for deploy

--- src/Job.js
+++ src/Job.js
@@ -9,7 +9,7 @@ const Job = ({ job }) => {
 
   return currUser ? (
     <div className="Job">
-      <h3 className="Job-title">{job.title}</h3>
+      <h3 className="Job-title ml-3">{job.title}</h3>
       <h5 className="Job-company">
         <Link to={`/companies/${job.companyHandle}`}>{job.companyName}</Link>
       </h5>
@@ -17,10 +17,10 @@ const Job = ({ job }) => {
       <span className="Job-equity">equity: {job.equity ? job.equity : 0}</span>
 
       {currUser.applications.includes(job.id) ? (
-        <button className="btn btn-success">Applied!</button>
+        <button className="btn btn-success mr-3">Applied!</button>
       ) : (
         <button
-          className="btn btn-primary"
+          className="btn btn-primary mr-3"
           onClick={() => {
             console.log(currUser.username, job.id);
             apply(currUser.username, job.id);
